c1ed14cb96957e3466eb537f49cc981d31d895bb
dheeraj-constructn <151025540+dheeraj-constructn@users.noreply.github.com>
2024-03-11 14:38:05 +0530

    initalization

---
+++ Sources/sandpack/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Meela Dheeraj on 11/03/24.
+//
+
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+public struct SwiftUIView: View {
+    public init(){}
+    public var body: some View {
+        Text("this is new package")
+    }
+}
+
+@available(iOS 13.0.0, *)
+#Preview {
+    SwiftUIView()
+}
